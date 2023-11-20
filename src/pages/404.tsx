16c2c3eff8cec557959484272f471e5360c98896
Tommy Ludwig <8924140+shakuzen@users.noreply.github.com>
2023-11-20 21:28:20 +0900

    Workaround direct access to /docs sub-paths
    Uses the same workaround we use on micrometer-docs today, adapted for the embedded setup.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link, HeadFC, PageProps } from "gatsby";
+import { Link, HeadFC, PageProps, Script } from "gatsby";
 
 const pageStyles = {
   color: "#232129",
@@ -24,12 +24,48 @@ const codeStyles = {
 };
 
 const browser = typeof window !== "undefined" && window;
+const isRedirectingDocs : boolean = browser && window.location.pathname.split('/')[1] === 'docs';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     browser && (
       <main style={pageStyles}>
-        <h1 style={headingStyles}>Page not found</h1>
+        <h1 style={headingStyles}>{ isRedirectingDocs ? 'Redirecting' : 'Page not found' }</h1>
+        <Script id="redirect-docs-script">
+          {`
+            // Single Page Apps for GitHub Pages
+            // MIT License
+            // https://github.com/rafgraph/spa-github-pages
+            // This script takes the current url and converts the path and query
+            // string into just a query string, and then redirects the browser
+            // to the new url with only a query string and hash fragment,
+            // e.g. https://www.foo.tld/one/two?a=b&c=d#qwe, becomes
+            // https://www.foo.tld/?/one/two&a=b~and~c=d#qwe
+            // Note: this 404.html file must be at least 512 bytes for it to work
+            // with Internet Explorer (it is currently > 512 bytes)
+
+            // If you're creating a Project Pages site and NOT using a custom domain,
+            // then set pathSegmentsToKeep to 1 (enterprise users may need to set it to > 1).
+            // This way the code will only replace the route part of the path, and not
+            // the real directory in which the app resides, for example:
+            // https://username.github.io/repo-name/one/two?a=b&c=d#qwe becomes
+            // https://username.github.io/repo-name/?/one/two&a=b~and~c=d#qwe
+            // Otherwise, leave pathSegmentsToKeep as 0.
+            var pathSegmentsToKeep = 1;
+
+            var l = window.location;
+            if (l.pathname.split('/')[1] === 'docs') {
+              l.replace(
+                l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+                l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
+                l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+                (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+                l.hash
+              );
+            }
+          `}
+        </Script>
+        { !isRedirectingDocs ? (
         <p style={paragraphStyles}>
           Sorry 😔, we couldn’t find what you were looking for.
           <br />
@@ -43,6 +79,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
           <br />
           <Link to="/">Go home</Link>.
         </p>
+          ) : null }
       </main>
     )
   );
