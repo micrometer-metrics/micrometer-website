46e71b7a009ee2ecb4203705d975555a4b2d8a6e
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2024-09-12 13:32:34 -0700

    Revert SpringOne banner
    See 3b4112141765e9641c327832c5068257535d513c
    See f40ec1c0552d21bb9c85a63f70f5512d7fd44171

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,9 @@
 import * as React from "react"
 import Navbar from "./Navbar"
-import Banner from "./Banner"
 
 const Header = () => {
   return (
     <div className="header">
-      <Banner />
       <Navbar />
     </div>
   )
